aeae500e91c51bf018606a861a75c7a5ae67cbda
saimprojects <saimpkf@gmail.com>
2025-10-26 10:50:37 +0500

    API file Link update

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 from .models import Product
 
-
 class ProductSerializer(serializers.ModelSerializer):
+    file = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ["id", "title", "slug", "description", "price", "file", "is_active", "external_link",]
+        fields = ["id", "title", "slug", "description", "price", "file", "is_active", "external_link"]
         read_only_fields = ["id", "slug"]
+
+    def get_file(self, obj):
+        if obj.file:
+            # agar Cloudinary use ho raha hai, full URL Cloudinary ke domain ke sath return kare
+            if str(obj.file).startswith("http"):
+                return obj.file
+            return f"https://res.cloudinary.com/dxommxt6d/{obj.file}"
+        return None
